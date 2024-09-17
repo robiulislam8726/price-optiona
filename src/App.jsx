2786a1861fc17b193761812d7c253a6460391097
robiulislam8726 <robiul.islam8726@gmail.com>
2024-09-17 15:28:18 +0900

    added price options code basic format

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-7xl bg-rose-600'>Price Options</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
