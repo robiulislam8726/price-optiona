e112aead56d5a82150b76438a7507d00bfd76093
robiulislam8726 <robiul.islam8726@gmail.com>
2024-09-17 20:35:44 +0900

    added frap second part

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio } from 'react-loader-spinner'
 
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
         // .then (res => res.json())
@@ -24,11 +26,24 @@ const Phones = () => {
                     return obj;
                 })
                 console.log(phonesWithFakeData);
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false);
             });
     }, [])
     return (
         <div>
+            {loading && <div>
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color='green'
+                    ariaLabel='three-dots-loading'
+                    wrapperStyle
+                    wrapperClass
+
+                />
+            </div>}
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
